46665014a1d7ed0aa407a109ad6f1276056c032f
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2022-08-16 02:43:21 +0530

    languages added

--- royal.js
+++ royal.js
@@ -4,29 +4,50 @@ const display = document.getElementById("display");
 const pauseBtn = document.getElementById("pause");
 const playBtn = document.getElementById("play");
 const login = document.getElementById("login");
+const language = document.getElementById("language");
+const lang = document.getElementById("lang");
+const navbar = document.getElementsByClassName("navbar");
+const bgBlack = document.getElementById("logbg");
+
 
 bg.style.display = "none";
+bgBlack.style.display = "none";
+lang.style.display = "none";
 
-visible = function () {
+visibleLogin = function () {
   if (bg.style.display == "block") {
     bg.style.display = "none";
     login.style.color = "white";
+    bgBlack.style.display = "none";
     display.style.display = "block";
-   
   } else {
     bg.style.display = "block";
     login.style.color = "red";
+    bgBlack.style.display = "block";
     display.style.display = "none";
   }
 };
+visibleLanguage = function(){
+ if(lang.style.display == "none"){
+    lang.style.display = "block";
+    display.style.display = "none";
+    language.style.color = "red";
+ }else{
+    lang.style.display = "none";
+    display.style.display = "block";
+    language.style.color = "white";
+ }
+}
 
+
+var a = true;
 function stop() {
-  video.pause();
-  pauseBtn.style.display = "none";
-  playBtn.style.display = "block";
+  if (a == true) {
+    video.pause();
+    a = false;
+  } else {
+    video.play();
+    a = true;
+  }
 }
-function resume() {
-  video.play();
-  pauseBtn.style.display = "block";
-  playBtn.style.display = "none";
-}
+
