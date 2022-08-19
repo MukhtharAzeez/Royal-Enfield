a0e2644abd1622c500ed601839d09ea5fd8e802e
Mukhthar Azeez <mukhtharazeez28@gmail.com>
2022-08-19 07:21:02 +0530

    comments removed

--- javaScript.js
+++ javaScript.js
@@ -8,7 +8,8 @@ const language = document.getElementById("language");
 const lang = document.getElementById("lang");
 const navbar = document.getElementsByClassName("navbar");
 const bgBlack = document.getElementById("logbg");
-
+const arrow1 = document.getElementById("down-arrow1");
+const arrow2 = document.getElementById("down-arrow2");
 bg.style.display = "none";
 bgBlack.style.display = "none";
 lang.style.display = "none";
@@ -20,12 +21,15 @@ visibleLogin = function () {
     login.style.color = "white";
     bgBlack.style.display = "none"; 
     display.style.display = "block";
+    arrow1.style.transform = "rotate(0deg)";
+    arrow1.style.color = "white";
   } else {
     bg.style.display = "block";
     login.style.color = "red";
     bgBlack.style.display = "block";
     display.style.display = "none";
-
+    arrow1.style.transform = "rotate(180deg)";
+    arrow1.style.color = "red";
     // language page remove
     lang.style.display = "none";
     language.style.color = "white";
@@ -36,7 +40,8 @@ visibleLanguage = function () {
     lang.style.display = "block";
     display.style.display = "none";
     language.style.color = "red";
-
+    arrow2.style.transform = "rotate(180deg)";
+    arrow2.style.color = "red";
     // login page remove
     bg.style.display = "none";
     login.style.color = "white";
@@ -46,6 +51,8 @@ visibleLanguage = function () {
     lang.style.display = "none";
     display.style.display = "block";
     language.style.color = "white";
+    arrow2.style.transform = "rotate(0deg)";
+    arrow2.style.color = "white";
   }
 };
 
